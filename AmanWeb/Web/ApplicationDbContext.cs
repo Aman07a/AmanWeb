7189119a0b74f29906f428daafa442dc883b53dc
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-03 13:29:12 +0100

    ApplicationDbContext

---
+++ AmanWeb/Web/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using AmanWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmanWeb.Web
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public DbSet<Category> Category { get; set; }
+	}
+}
