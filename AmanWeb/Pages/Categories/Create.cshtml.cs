1969eb15cecd4abddea5d49cd07625e8cda4bb1b
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-03 15:52:46 +0100

    Create Property in Page Model

--- AmanWeb/Pages/Categories/Create.cshtml.cs
+++ AmanWeb/Pages/Categories/Create.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AmanWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,8 @@ namespace AmanWeb.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+        public Category Category { get; set; }
+
         public void OnGet()
         {
         }
