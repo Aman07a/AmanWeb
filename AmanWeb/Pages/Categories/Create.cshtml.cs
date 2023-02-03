a40366550df299e2390d2b86ce5c62bc4e1a76a1
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-03 16:02:41 +0100

    Create Category and Bind Property

--- AmanWeb/Pages/Categories/Create.cshtml.cs
+++ AmanWeb/Pages/Categories/Create.cshtml.cs
@@ -2,18 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AmanWeb.Data;
 using AmanWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AmanWeb.Pages.Categories
 {
+	[BindProperties]
     public class CreateModel : PageModel
     {
-        public Category Category { get; set; }
+        private readonly ApplicationDbContext _db;
 
-        public void OnGet()
+		public Category Category { get; set; }
+
+		public CreateModel(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public void OnGet()
         {
         }
+
+		public async Task<IActionResult> OnPost() {
+			await _db.Category.AddAsync(Category);
+			await _db.SaveChangesAsync();
+			return RedirectToPage("Index");
+		}
     }
 }
