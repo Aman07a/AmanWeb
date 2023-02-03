422978416be5982b8816dd9836f5b04e93f0cde4
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-03 13:10:22 +0100

    Data Annotations

--- AmanWeb/Models/Category.cs
+++ AmanWeb/Models/Category.cs
@@ -1,9 +1,15 @@
-﻿namespace AmanWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmanWeb.Models
 {
 	public class Category
 	{
+		[Key]
 		public int Id { get; set; }
+
+		[Required]
 		public string Name { get; set; }
+
 		public int DisplayOrder { get; set; }
 	}
 }
